9ba1a25f97d2f70c1f20426382d9f3f1d1ed1d56
Administrator <Administrator@DESKTOP-4EMBS87>
2022-12-25 18:16:50 +0800

    自动装配类的加载顺序

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/AutoConfigurationSorter.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/AutoConfigurationSorter.java
@@ -16,20 +16,13 @@
 
 package org.springframework.boot.autoconfigure;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 import org.springframework.core.type.AnnotationMetadata;
 import org.springframework.core.type.classreading.MetadataReader;
 import org.springframework.core.type.classreading.MetadataReaderFactory;
 import org.springframework.util.Assert;
+
+import java.io.IOException;
+import java.util.*;
 
 /**
  * Sort {@link EnableAutoConfiguration auto-configuration} classes into priority order by
@@ -57,14 +50,18 @@ class AutoConfigurationSorter {
 				this.autoConfigurationMetadata, classNames);
 		List<String> orderedClassNames = new ArrayList<>(classNames);
 		// Initially sort alphabetically
+		//首先按照字母升序排序
 		Collections.sort(orderedClassNames);
 		// Then sort by order
+		// 按照@AutoConfigure Order指定的顺序排续
 		orderedClassNames.sort((o1, o2) -> {
+			//如果配置类没有指定order,则getOrder方法将会给与默认值0
 			int i1 = classes.get(o1).getOrder();
 			int i2 = classes.get(o2).getOrder();
 			return Integer.compare(i1, i2);
 		});
 		// Then respect @AutoConfigureBefore @AutoConfigureAfter
+		// 接下来按照@AutoConfigureBefore @AutoConfigureAfter注解调整顺序
 		orderedClassNames = sortByAnnotation(classes, orderedClassNames);
 		return orderedClassNames;
 	}
@@ -84,16 +81,22 @@ class AutoConfigurationSorter {
 	private void doSortByAfterAnnotation(AutoConfigurationClasses classes, List<String> toSort, Set<String> sorted,
 			Set<String> processing, String current) {
 		if (current == null) {
+			//从要排序集合删除第一个排序类
 			current = toSort.remove(0);
 		}
+		//将正在处理类添加到集合
 		processing.add(current);
+		//getClassesRequestedAfter方法获取指定类的after、before集合的并集
 		for (String after : classes.getClassesRequestedAfter(current)) {
 			checkForCycles(processing, current, after);
+			//通过递归的方式将当前类的after、before类添加到已排序集合
 			if (!sorted.contains(after) && toSort.contains(after)) {
 				doSortByAfterAnnotation(classes, toSort, sorted, processing, after);
 			}
 		}
+		//将正在处理类添加到集合
 		processing.remove(current);
+		//将处理过的类添加到已处理集合
 		sorted.add(current);
 	}
 
@@ -119,12 +122,16 @@ class AutoConfigurationSorter {
 				AutoConfigurationMetadata autoConfigurationMetadata, Collection<String> classNames, boolean required) {
 			for (String className : classNames) {
 				if (!this.classes.containsKey(className)) {
+					//获取自动化配置类实例对象
 					AutoConfigurationClass autoConfigurationClass = new AutoConfigurationClass(className,
 							metadataReaderFactory, autoConfigurationMetadata);
+					//判定配置类是否可用
 					boolean available = autoConfigurationClass.isAvailable();
 					if (required || available) {
+						//将符合条件的配置类加入到配置类集合
 						this.classes.put(className, autoConfigurationClass);
 					}
+					//如果当前类有效，递归处理@AutoConfigureBefore、@AutoConfigureAfter注解指定的类
 					if (available) {
 						addToClasses(metadataReaderFactory, autoConfigurationMetadata,
 								autoConfigurationClass.getBefore(), false);
@@ -135,11 +142,14 @@ class AutoConfigurationSorter {
 			}
 		}
 
+		//获取指定类的自动化配置类对象
 		AutoConfigurationClass get(String className) {
 			return this.classes.get(className);
 		}
 
+		//获取指定类after、before类的集合
 		Set<String> getClassesRequestedAfter(String className) {
+			//获取指定类after属性对应的集合
 			Set<String> classesRequestedAfter = new LinkedHashSet<>(get(className).getAfter());
 			this.classes.forEach((name, autoConfigurationClass) -> {
 				if (autoConfigurationClass.getBefore().contains(className)) {
@@ -153,6 +163,7 @@ class AutoConfigurationSorter {
 
 	private static class AutoConfigurationClass {
 
+		//配置类名
 		private final String className;
 
 		private final MetadataReaderFactory metadataReaderFactory;
@@ -161,8 +172,10 @@ class AutoConfigurationSorter {
 
 		private volatile AnnotationMetadata annotationMetadata;
 
+		//配置类的before类集合
 		private volatile Set<String> before;
 
+		//配置类的after集合
 		private volatile Set<String> after;
 
 		AutoConfigurationClass(String className, MetadataReaderFactory metadataReaderFactory,
@@ -174,16 +187,19 @@ class AutoConfigurationSorter {
 
 		boolean isAvailable() {
 			try {
+				//判定当前配置类是否已经被自动化配置处理器类处理过
 				if (!wasProcessed()) {
+					//如果没有被自动化配置处理器处理过，则通过工厂方法获取注解元数据对象
 					getAnnotationMetadata();
 				}
 				return true;
-			}
-			catch (Exception ex) {
+			} catch (Exception ex) {
 				return false;
 			}
 		}
 
+		//获取@AutoConfigureBefore注解指定的值（通过注解元数据方式获取）
+		//如果不存在，则通过工厂方法获取注解元数据，然后获取注解属性值
 		Set<String> getBefore() {
 			if (this.before == null) {
 				this.before = (wasProcessed() ? this.autoConfigurationMetadata.getSet(this.className,
@@ -192,6 +208,8 @@ class AutoConfigurationSorter {
 			return this.before;
 		}
 
+		//获取@AutoConfigureAfter注解指定的值（通过注解元数据方式获取）
+		//如果不存在，则通过工厂方法获取注解元数据，然后获取注解属性值
 		Set<String> getAfter() {
 			if (this.after == null) {
 				this.after = (wasProcessed() ? this.autoConfigurationMetadata.getSet(this.className,
@@ -200,6 +218,7 @@ class AutoConfigurationSorter {
 			return this.after;
 		}
 
+		//获取配置类order值，默认值是：0
 		private int getOrder() {
 			if (wasProcessed()) {
 				return this.autoConfigurationMetadata.getInteger(this.className, "AutoConfigureOrder",
@@ -210,11 +229,13 @@ class AutoConfigurationSorter {
 			return (attributes != null) ? (Integer) attributes.get("value") : AutoConfigureOrder.DEFAULT_ORDER;
 		}
 
+		//判定类是否正在被注解处理器处理
 		private boolean wasProcessed() {
 			return (this.autoConfigurationMetadata != null
 					&& this.autoConfigurationMetadata.wasProcessed(this.className));
 		}
 
+		//获取注解的属性值
 		private Set<String> getAnnotationValue(Class<?> annotation) {
 			Map<String, Object> attributes = getAnnotationMetadata().getAnnotationAttributes(annotation.getName(),
 					true);
@@ -227,13 +248,13 @@ class AutoConfigurationSorter {
 			return value;
 		}
 
+		//获取类的MetadataReader对象
 		private AnnotationMetadata getAnnotationMetadata() {
 			if (this.annotationMetadata == null) {
 				try {
 					MetadataReader metadataReader = this.metadataReaderFactory.getMetadataReader(this.className);
 					this.annotationMetadata = metadataReader.getAnnotationMetadata();
-				}
-				catch (IOException ex) {
+				} catch (IOException ex) {
 					throw new IllegalStateException("Unable to read meta-data for class " + this.className, ex);
 				}
 			}
