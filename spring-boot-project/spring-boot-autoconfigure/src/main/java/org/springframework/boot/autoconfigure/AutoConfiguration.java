9e56d18a00a10e81d16f1be41263fa6c4ae12fcc
Administrator <Administrator@DESKTOP-4EMBS87>
2022-12-25 18:16:50 +0800

    自动装配类的加载顺序

--- spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/AutoConfiguration.java
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/AutoConfiguration.java
@@ -16,12 +16,6 @@
 
 package org.springframework.boot.autoconfigure;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.annotation.ImportCandidates;
@@ -31,12 +25,15 @@ import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.AliasFor;
 
+import java.lang.annotation.*;
+
 /**
  * Indicates that a class provides configuration that can be automatically applied by
  * Spring Boot. Auto-configuration classes are regular
  * {@link Configuration @Configuration} with the exception that
  * {@literal Configuration#proxyBeanMethods() proxyBeanMethods} is always {@code false}.
  * They are located using {@link ImportCandidates}.
+ * 指示类提供可由Spring Boot自动应用的配置。
  * <p>
  * Generally auto-configuration classes are marked as {@link Conditional @Conditional}
  * (most often using {@link ConditionalOnClass @ConditionalOnClass} and
@@ -63,12 +60,14 @@ public @interface AutoConfiguration {
 	 * Explicitly specify the name of the Spring bean definition associated with the
 	 * {@code @AutoConfiguration} class. If left unspecified (the common case), a bean
 	 * name will be automatically generated.
+	 * 显式指定与AutoConfiguration类关联的Spring bean定义的名称。如果未指定（常见情况），将自动生成bean名称。
 	 * <p>
 	 * The custom name applies only if the {@code @AutoConfiguration} class is picked up
 	 * through component scanning or supplied directly to an
 	 * {@link AnnotationConfigApplicationContext}. If the {@code @AutoConfiguration} class
 	 * is registered as a traditional XML bean definition, the name/id of the bean element
 	 * will take precedence.
+	 *
 	 * @return the explicit component name, if any (or empty String otherwise)
 	 * @see AnnotationBeanNameGenerator
 	 */
@@ -77,6 +76,7 @@ public @interface AutoConfiguration {
 
 	/**
 	 * The auto-configure classes that should have not yet been applied.
+	 * 尚未应用的自动配置类。
 	 * @return the classes
 	 */
 	@AliasFor(annotation = AutoConfigureBefore.class, attribute = "value")
@@ -84,6 +84,7 @@ public @interface AutoConfiguration {
 
 	/**
 	 * The names of the auto-configure classes that should have not yet been applied.
+	 * 尚未应用的自动配置类的名称
 	 * @return the class names
 	 */
 	@AliasFor(annotation = AutoConfigureBefore.class, attribute = "name")
@@ -91,6 +92,7 @@ public @interface AutoConfiguration {
 
 	/**
 	 * The auto-configure classes that should have already been applied.
+	 * 应已应用的自动配置类
 	 * @return the classes
 	 */
 	@AliasFor(annotation = AutoConfigureAfter.class, attribute = "value")
@@ -98,6 +100,7 @@ public @interface AutoConfiguration {
 
 	/**
 	 * The names of the auto-configure classes that should have already been applied.
+	 * 应已应用的自动配置类名称
 	 * @return the class names
 	 */
 	@AliasFor(annotation = AutoConfigureAfter.class, attribute = "name")
