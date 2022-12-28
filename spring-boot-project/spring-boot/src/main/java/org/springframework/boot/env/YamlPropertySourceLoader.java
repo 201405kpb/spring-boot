0b9ffe139712c1fb92c3b0aaf571690f844b6a64
Administrator <Administrator@DESKTOP-4EMBS87>
2022-12-28 19:43:18 +0800

    资源文件加载

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/env/YamlPropertySourceLoader.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/env/YamlPropertySourceLoader.java
@@ -16,18 +16,20 @@
 
 package org.springframework.boot.env;
 
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.ClassUtils;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.core.env.PropertySource;
-import org.springframework.core.io.Resource;
-import org.springframework.util.ClassUtils;
-
 /**
  * Strategy to load '.yml' (or '.yaml') files into a {@link PropertySource}.
+ * <p>
+ * 将“.yml”（或“.yaml”）文件加载到 PropertySource中的策略
  *
  * @author Dave Syer
  * @author Phillip Webb
@@ -43,14 +45,17 @@ public class YamlPropertySourceLoader implements PropertySourceLoader {
 
 	@Override
 	public List<PropertySource<?>> load(String name, Resource resource) throws IOException {
+		// 如果不存在 `org.yaml.snakeyaml.Yaml` 这个 Class 对象，则抛出异常
 		if (!ClassUtils.isPresent("org.yaml.snakeyaml.Yaml", getClass().getClassLoader())) {
 			throw new IllegalStateException(
 					"Attempted to load " + name + " but snakeyaml was not found on the classpath");
 		}
+		// 通过 Yaml 解析该文件资源
 		List<Map<String, Object>> loaded = new OriginTrackedYamlLoader(resource).load();
 		if (loaded.isEmpty()) {
 			return Collections.emptyList();
 		}
+		// 将上面获取到的 Map 集合们一一封装成 OriginTrackedMapPropertySource 对象
 		List<PropertySource<?>> propertySources = new ArrayList<>(loaded.size());
 		for (int i = 0; i < loaded.size(); i++) {
 			String documentNumber = (loaded.size() != 1) ? " (document #" + i + ")" : "";
